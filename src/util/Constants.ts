d2619ed96024d5f924e635f40cb17ae8b79fb0c3
Hasatori <hradil.o@email.cz>
2020-12-30 22:42:46 +0100

    Redirect uri updated.

--- src/util/Constants.ts
+++ src/util/Constants.ts
@@ -1,11 +1,11 @@
 export function GOOGLE_AUTH_URL(locale: string): string {
-    return process.env.REACT_APP_REST_API_URL + '/oauth2/authorize/google?redirect_uri=' + process.env.OAUTH2_REDIRECT_URI + '&language=' + locale
+    return process.env.REACT_APP_REST_API_URL + '/oauth2/authorize/google?redirect_uri=' + process.env.REACT_APP_OAUTH2_REDIRECT_URI + '&language=' + locale
 };
 
 export function FACEBOOK_AUTH_URL(locale: string): string {
-    return process.env.REACT_APP_REST_API_URL + '/oauth2/authorize/facebook?redirect_uri=' + process.env.OAUTH2_REDIRECT_URI + '&language=' + locale
+    return process.env.REACT_APP_REST_API_URL + '/oauth2/authorize/facebook?redirect_uri=' + process.env.REACT_APP_OAUTH2_REDIRECT_URI + '&language=' + locale
 };
 
 export function GITHUB_AUTH_URL(locale: string): string {
-    return process.env.REACT_APP_REST_API_URL + '/oauth2/authorize/github?redirect_uri=' + process.env.OAUTH2_REDIRECT_URI + '&language=' + locale
+    return process.env.REACT_APP_REST_API_URL + '/oauth2/authorize/github?redirect_uri=' + process.env.REACT_APP_OAUTH2_REDIRECT_URI + '&language=' + locale
 };
