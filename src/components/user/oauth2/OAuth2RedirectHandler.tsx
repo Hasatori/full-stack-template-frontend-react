ce9c75345f7970e2742793ba1b871fcfb5e9499f
Hasatori <hradil.o@email.cz>
2021-01-01 10:47:30 +0100

    Is mobile redirect removed.

--- src/components/user/oauth2/OAuth2RedirectHandler.tsx
+++ src/components/user/oauth2/OAuth2RedirectHandler.tsx
@@ -10,13 +10,13 @@ function OAuth2RedirectHandler(props: AppProps & RouteComponentProps) {
     const token = getUrlParameter(props.location.search, 'token');
     const expires = getUrlParameter(props.location.search, 'expires');
     const error = getUrlParameter(props.location.search, 'error');
-    if (isMobile) {
+    /*if (isMobile) {
         window.location.href = "exp://192.168.1.179:19000?token=" + token + "&expires=" + expires + "&error=" + error;
         return <Redirect to={{
             pathname: "/profile",
             state: {from: props.location}
         }}/>;
-    } else if (token) {
+    } else */if (token) {
         store.dispatch(successActionCreator('Logged in'));
         return <Redirect to={{
             pathname: "/profile",
