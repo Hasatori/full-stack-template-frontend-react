6218c721a6a09061ea11f7555ada2cc4deb8f0a4
Hasatori <hradil.o@email.cz>
2021-08-21 17:31:31 +0200

    Profile section refactored.

--- src/components/user/account/Account.tsx
+++ src/components/user/account/Account.tsx
@@ -6,25 +6,24 @@ import {connect} from "react-redux";
 import CloseAccount from "./CloseAccount";
 import Profile from "./Profile";
 import {useTranslation} from "react-i18next";
-import {MDBContainer} from "mdbreact";
+import {MDBCol, MDBContainer, MDBRow} from "mdbreact";
 
 
 function Account() {
     const {t} = useTranslation();
     return (
 
-        <div>
-            <div>
+        <MDBRow>
+            <MDBCol lg="1"/>
+            <MDBCol  md="12" lg="10">
                 <h5 className='text-center  '>{t('ns1:manageProfileHeading')}</h5>
                 <Profile {...{} as any}/>
-                <div className='divider'/>
                 <ChangePassword {...{} as any}/>
-                <div className='divider'/>
                 <TwoFactorSetup {...{} as any}/>
-                <div className='divider'/>
                 <CloseAccount/>
-            </div>
-        </div>
+            </MDBCol>
+            <MDBCol  lg="1"/>
+        </MDBRow>
     );
 
 }
