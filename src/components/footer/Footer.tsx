096b44a945ab5c537a994c86efc5c9742f82439b
Hasatori <hradil.o@email.cz>
2021-09-19 14:43:11 +0200

    Footer updated.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,18 +1,14 @@
-import {MDBContainer, MDBFooter} from "mdbreact";
+import {MDBFooter} from "mdbreact";
 import React from "react";
 import './Footer.css';
-import {store} from "../../index";
-import {INFO} from "../../redux/actiontype/GeneralActionTypes";
 import {useTranslation} from "react-i18next";
-import googlePlayBadge from "../../assets/images/mobilestore/google-play-badge.svg";
-import appStoreBadge from "../../assets/images/mobilestore/app-store-badge.svg";
 
 export function Footer() {
     const [t] = useTranslation();
     return (
         <MDBFooter className="footer">
-            <div className="footer-copyright text-center py-3 ">
-                &copy; {new Date().getFullYear()} Copyright: <strong>fullstack-template-react.herokuapp.com</strong>
+            <div className="footer-copyright text-center py-3 ">Copyright
+                &copy;{new Date().getFullYear()}<strong>Hasatori</strong>
             </div>
         </MDBFooter>
     )
